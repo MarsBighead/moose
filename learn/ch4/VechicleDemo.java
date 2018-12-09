831b8a64fc055fd9ae8ee2687884d74d14066fac
MarsBighead <duanmhy@126.com>
2018-12-09 13:57:27 +0800

    Add one class object

--- learn/ch4/VechicleDemo.java
+++ learn/ch4/VechicleDemo.java
@@ -7,13 +7,21 @@ class Vechicle {
 class VechicleDemo {
 	public static void main(String args[]){
 		Vechicle minivan = new Vechicle();
-		int range;
+		Vechicle sportcar = new Vechicle();
+		int range,range1;
+
 		minivan.passengeers = 7;
 		minivan.fuelcap = 16;
 		minivan.mpg =21;
 
+		sportcar.passengeers = 2;
+		sportcar.fuelcap = 14;
+		sportcar.mpg =12;
+
 		range = minivan.fuelcap * minivan.mpg;
+		range1 = sportcar.fuelcap * sportcar.mpg;
 		System.out.println("Minivan can carry "+ minivan.passengeers + " with a range of "+range);
+		System.out.println("Sportcar can carry "+ sportcar.passengeers + " with a range of "+range1);
 
 	}
 }
