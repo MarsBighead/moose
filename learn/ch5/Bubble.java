fb86a27489ab2d443423eeae420c6104c98a4b90
MarsBighead <duanmhy@126.com>
2018-12-09 16:01:27 +0800

    Add Bubble sort for array data type in Java

---
+++ learn/ch5/Bubble.java
@@ -0,0 +1,30 @@
+class Bubble {
+	public static void main(String args[]){
+		int nums[] = {99, -10, 100213, 18, -978, 5623, 463, -9, 287, 49};
+		int a, b, t;
+		int size;
+		size =10;
+		//display original array
+		System.out.println("Original array is:");
+		for (int i=0; i<size; i++)
+			System.out.print(" " + nums[i]);
+		System.out.println();
+
+		// This is the Bubble sort.
+		for(a=1; a<size; a++)
+			for(b=size-1; b>=a; b--)
+				if(nums[b-1] > nums[b]) { // if out of order
+					// exchange elements
+					t = nums[b-1];
+					nums[b-1]=nums[b];
+					nums[b]=t;
+				}
+		
+		//display sorted array
+		System.out.println("Sorted array is:");
+		for (int i=0; i<size; i++)
+			System.out.print(" " + nums[i]);
+		System.out.println();
+
+	}
+}
