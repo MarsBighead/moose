cc3ff66f7ec23bd2a78b153ade891a9c80942b09
MarsBighead <duanmhy@126.com>
2018-12-09 14:25:41 +0800

    Change method range with returned value

--- learn/ch4/VechicleDemo.java
+++ learn/ch4/VechicleDemo.java
@@ -3,8 +3,9 @@ class Vechicle {
 	int fuelcap;
 	int mpg; // fuel assumption in miles per gallon
 
-	void range (){
-		System.out.println("Range is " + fuelcap * mpg);
+	int range (){
+		//System.out.println("Range is " + fuelcap * mpg);
+		return fuelcap * mpg;
 	}
 }
 
@@ -12,7 +13,6 @@ class VechicleDemo {
 	public static void main(String args[]){
 		Vechicle minivan = new Vechicle();
 		Vechicle sportcar = new Vechicle();
-		int range,range1;
 
 		minivan.passengeers = 7;
 		minivan.fuelcap = 16;
@@ -22,10 +22,10 @@ class VechicleDemo {
 		sportcar.fuelcap = 14;
 		sportcar.mpg =12;
 
-		System.out.print("Minivan can carry "+ minivan.passengeers + ". "); 
-		minivan.range();
-		System.out.print("Sportcar can carry "+ sportcar.passengeers + ". ");
-		sportcar.range();
+		System.out.println("Minivan can carry "+ minivan.passengeers + 
+				" with range of " + minivan.range() + " miles."); 
+		System.out.println("Sportcar can carry "+ sportcar.passengeers + 
+				" with range of " + sportcar.range() + " miles."); 
 
 	}
 }
